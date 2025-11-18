1e61e2e7a7a11af4a1f72f97e781096e823f2a9c
toddupshawse <toddupshawse@gmail.com>
2025-11-18 17:26:46 -0500

    Refactor address selection event handlers

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
-const homeMsg = "You selected the Home address";
-const workMsg = "You selected the Work address";
+let homeMsg = "You selected the Home address";
+let workMsg = "You selected the Work address";
 
-document.getElementById("homeoption").addEventListener("click", function() {
+document.getElementById("homeRadio").addEventListener("click", function () {
     alert(homeMsg);
-    document.getElementById("streetinput").value = "100 Main St.";
-    document.getElementById("cityinput").value = "Sicilia";
-    document.getElementById("stateinput").value = "MA";
-    document.getElementById("zipinput").value = "02103";
+
+    document.getElementById("street").value = "100 Main St.";
+    document.getElementById("city").value = "Virginia Beach";
+    document.getElementById("state").value = "VA";
+    document.getElementById("zip").value = "23450";
 });
 
-document.getElementById("workoption").addEventListener("click", function() {
+document.getElementById("workRadio").addEventListener("click", function () {
     alert(workMsg);
-    document.getElementById("streetinput").value = "123 Robin Ln.";
-    document.getElementById("cityinput").value = "Crab City";
-    document.getElementById("stateinput").value = "MA";
-    document.getElementById("zipinput").value = "02104";
+
+    document.getElementById("street").value = "500 Business Plaza";
+    document.getElementById("city").value = "Norfolk";
+    document.getElementById("state").value = "VA";
+    document.getElementById("zip").value = "23510";
 });
